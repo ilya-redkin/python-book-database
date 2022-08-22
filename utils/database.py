a243330cf4c4079521523db7b8ae41b9d03ab4ae
Ilya Redkin <ilya.redking@gmail.com>
2022-08-22 15:41:14 -0400

    first commit

---
+++ utils/database.py
@@ -0,0 +1,30 @@
+from .database_connection import DatabaseConnection
+
+
+def create_book_table() -> None:
+    with DatabaseConnection("data.db") as connection:
+        cursor = connection.cursor()
+        cursor.execute("CREATE TABLE IF NOT EXISTS books(name text primary key, author text, read integer)")
+
+def add_book(name, author) -> None:
+    with DatabaseConnection("data.db") as connection:
+        cursor = connection.cursor()
+        cursor.execute("INSERT INTO books(name, author, read) VALUES (?, ?, 0)", (name, author))
+       
+
+def get_all_books():
+    with DatabaseConnection("data.db") as connection:
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM books")
+        books = [{"name": row[0], "author": row[1], "read": row[2]} for row in cursor.fetchall()]
+        return books
+
+def mark_book_as_read(name: str) -> None:
+    with DatabaseConnection("data.db") as connection:
+        cursor = connection.cursor()
+        cursor.execute("UPDATE books SET read=1 WHERE name =?", (name,))
+
+def delete_book(name) -> None:
+    with DatabaseConnection("data.db") as connection:
+        cursor = connection.cursor()
+        cursor.execute("DELETE FROM books WHERE name=?", (name,))
